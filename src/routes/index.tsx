2a1035bb6c1358ceb5eebe6248a50bdc1ad79afb
Bruno Macedo <devbrunomacedo@gmail.com>
2026-06-07 09:17:51 -0300

    feat: adiciona rota de cadastro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../pages/Home";
 import { LoginPage } from "../pages/Login";
-import { SignUpPage } from "../pages/SignUp";
+import { RegisterPage } from "../pages/Register";
 /* import { Layout } from "../components/Layout"*/
 
 export const router = createBrowserRouter([
@@ -21,8 +21,8 @@ export const router = createBrowserRouter([
                         element: <LoginPage />
                     },
                     {
-                        path: 'signup',
-                        element: <SignUpPage />
+                        path: 'register',
+                        element: <RegisterPage />
                     }
                 ]
             },
