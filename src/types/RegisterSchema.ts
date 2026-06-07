164b75f52e17e5a6b8a7c553b04631f25f5213d4
Bruno Macedo <devbrunomacedo@gmail.com>
2026-06-07 09:20:07 -0300

    feat: add pagina cadastro com base na estrutura de login

--- src/types/RegisterSchema.ts
+++ src/types/RegisterSchema.ts
@@ -1,19 +1,19 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-    name: z.string()
+    name: z
+        .string()
         .min(1, "O nome é obrigatório.")
         .refine((value) => value.length >= 3, {
-            message: "O nome deve ter no mínimo 3 caracteres."
-        })
-        ,
+            message: "O nome deve ter no mínimo 3 caracteres.",
+        }),
     lastName: z.string().min(1, "O sobrenome é obrigatório."),
     phone: z
         .string()
         .min(1, "O telefone é obrigatório.")
         .transform((value) => value.replace(/\D/g, ""))
         .refine((value) => value.length === 11, {
-            message: "Telefone inválido"
+            message: "Telefone inválido",
         }),
     email: z
         .string()
