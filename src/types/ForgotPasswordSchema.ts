e587b634f8eae9effec02ba9eea55e00a0863707
Bruno Macedo <devbrunomacedo@gmail.com>
2026-06-07 10:15:41 -0300

    feat: add schema de validacao para a pagina ForgotPassword

---
+++ src/types/ForgotPasswordSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const forgotPasswordSchema = z.object({
+    email: z
+        .string()
+        .min(1, "O e-mail é obrigatório.")
+        .email("Digite um formato de e-mail válido."),
+});
+
+export type ForgotPasswordFormData = z.infer<typeof forgotPasswordSchema>;
+    
